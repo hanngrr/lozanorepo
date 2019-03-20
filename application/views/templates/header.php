bc15b2f393a91b2b9b0afd24ec342ac21d59048c
it3r4-lozano <nanalou1013@gmail.com>
2019-03-20 19:33:48 +0800

    edited commit

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,6 +4,8 @@
 	<title>My First GitRepo</title>
 	<link rel="stylesheet" type="text/css" href="<?php  echo base_url("assets/vendor/bootstrap/css/bootstrap.min.css"); ?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css"); ?>">
+
+    <script type="text/javascript" href="<?php echo base_url("assets/js/style.js") ?>"></script>
 	
 </head>
 <body>
