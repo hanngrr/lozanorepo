9be93fbdfd6c3eeaa6dbff2c4b8ee34594ff645b
it3r4-lozano <nanalou1013@gmail.com>
2019-03-20 19:31:14 +0800

    my first repository

---
+++ application/views/templates/body.php
@@ -0,0 +1,3 @@
+<div>
+	<h1><center><b>WELCOME TO MY WEBPAGE</b></center></h1>
+</div>
---
+++ application/views/templates/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My First GitRepo</title>
+	<link rel="stylesheet" type="text/css" href="<?php  echo base_url("assets/vendor/bootstrap/css/bootstrap.min.css"); ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url("assets/css/style.css"); ?>">
+	
+</head>
+<body>
+
+
+
